0c8f8136ba1bdd2a1597ee4b492b56c5bf9df616
sourav711 <105282782+sourav711@users.noreply.github.com>
2022-07-08 13:36:47 +0530

    Delete finalll directory

--- finalll/cg_docs/cg_docs/Resources/Documents/country.sql
+++
@@ -1,9 +0,0 @@
-create database Cricket
-USE Cricket
-GO
-create table Country
-( Country_Id int primary key identity(1,1),
-Country_Name nvarchar(30),
-Continent nvarchar(30),
-Country_Code nvarchar(20)
-)
--- finalll/cg_docs/cg_docs/Resources/Documents/createplayerInfo.sql
+++
@@ -1,7 +0,0 @@
-go
-create table PlayersInfo
-(Player_Id int primary key identity(1,1),
-Player_Name varchar(30) not null,
-Player_Age int not null,
-Player_Role varchar(40),
-Player_Matches int);
